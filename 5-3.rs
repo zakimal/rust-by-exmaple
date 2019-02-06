25a8377cbffccf4ef8a368c03dd91d59bbe51708
Ozaki Yoichi <zak@nli.ics.keio.ac.jp>
2019-02-06 19:25:23 +0900

    add 5-3.rs

---
+++ 5-3.rs
@@ -0,0 +1,13 @@
+type NanoSecond = u64;
+type Inch = u64;
+
+#[allow(non_camel_case_types)]
+type u64_t = u64;
+
+#[allow(trivial_numeric_casts)]
+fn main() {
+    // 型名にaliasを設定することもできるけど、CamelCaseでないとならない。
+    let nanoseconds: NanoSecond = 5 as u64_t;
+    let inches: Inch = 2 as u64_t;
+    println!("{} nanoseconds + {} inches = {} unit?", nanoseconds, inches, nanoseconds + inches);
+}
